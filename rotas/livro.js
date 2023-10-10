4317a892a2f128898d7475fca5a38a81f7c07d70
MariLuzRibeiro <124590264+MariLuzRibeiro@users.noreply.github.com>
2023-10-10 09:19:17 -0300

    status http

--- rotas/livro.js
+++ rotas/livro.js
@@ -3,7 +3,13 @@ const { Router } = require("express")
 const router = Router()
 
 router.get('/', (req, res) => {
-    res.send("TesteTesteOK")
+    try{
+        throw new Error("teste")
+        res.send("TesteTesteOK")
+    }catch (error){
+        res.status(500)
+        res.send(error.message)
+    }
 }) 
 
 router.post('/', (req, res) => {
