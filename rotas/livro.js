b2e809bde7f851e3910a92a53b2de145cefe127b
MariLuzRibeiro <124590264+MariLuzRibeiro@users.noreply.github.com>
2023-10-10 14:12:04 -0300

    Post a partir do body

--- rotas/livro.js
+++ rotas/livro.js
@@ -1,5 +1,5 @@
 const { Router } = require("express")
-const { getLivros, getLivro } = require("../controladores/livro")
+const { getLivros, getLivro, postLivro } = require("../controladores/livro")
 
 const router = Router()
 
@@ -7,9 +7,7 @@ router.get('/', getLivros)
 
 router.get('/:id', getLivro) 
 
-router.post('/', (req, res) => {
-    res.send('Requisição do tipo Post')
-})
+router.post('/', postLivro)
 
 router.patch('/', (req, res) => {
     res.send('Requisição do tipo Patch')
